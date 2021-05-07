4191dbdc484f76198898394097d92726131fd294
A00517153 <57736675+A00517153@users.noreply.github.com>
2021-05-07 00:16:46 -0500

    Added changes:
    - Added 'simple_blur' kernel with function.
    
    - Added optional verbose argument for visualization control.
    
    - Added specific names to generated files for possible future comparison.
    
    - Normalized 'gaussian_blur' for output reasons, now it generates a workable image instead of white image.

--- README.md
+++ README.md
@@ -25,6 +25,13 @@ pip install numpy
 ```
 
 ## Funcionamiento
+Para que el programa corra le debera ingresar el path de la imagen que desea utilizar con los filtros en forma de argumento. Debera ingresarlo de la siguiente forma:
+```
+pyhton convolution.py -i image.jpg
+```
+
+
+
 En su estado actual, el programa toma la imagen *Testing.jpg* y genera su matriz a escala de grises, donde guarda esta matriz como imagen. Una vez hecho esto, se aplica una convolución usando el filtro proporcionado por el usuario al llamar la función *convolution*.
 
 El programa nos permite ver sus dimensiones, junto con las dimensiones del filtro kernel que será aplicado, y los tamaños de las matrices usadas junto con sus modificaciones de tamaño. Para asegurarnos que el procedimiento es correcto actualmente, se mantuvo un caso de prueba; se importa la imagen mencionada anteriormente (*Testing.jpg*) y se le aplica una convolución usando un kernel en la función declarada.
--- convolution.py
+++ convolution.py
@@ -4,80 +4,100 @@ import matplotlib.pyplot as plt
 import argparse
 
 
-def convolution(img, kernel, verbose = False):
+def convolution(img, kernel, verbose=False, filename="Output_kernel.jpg"):
     # Converts from a 3 channel image to a 2-D array in grayscale.
-	if len(img.shape) == 3:
-		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		cv2.imwrite("greyscale_image.jpg", img)
+    if len(img.shape) == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite("greyscale_image.jpg", img)
 
     # Gets the dimensions of the matrices of the image and the kernel.
-	img_row, img_col = img.shape
-	kernel_row, kernel_col = kernel.shape
+    img_row, img_col = img.shape
+    kernel_row, kernel_col = kernel.shape
 
     # Defines the size of padding and the padded image to work with.
-	padding = np.array([int((i - 1) / 2) for i in kernel.shape])
-	padded_img = np.zeros(padding*2+img.shape)
-	padded_img[padding[0]:-padding[0], padding[1]:-padding[1]] = img
+    padding = np.array([int((i - 1) / 2) for i in kernel.shape])
+    padded_img = np.zeros(padding*2+img.shape)
+    padded_img[padding[0]:-padding[0], padding[1]:-padding[1]] = img
 
     # Defines empty 2-D matrix of the size of the image as output matrix.
-	output = np.zeros(img.shape)
+    output = np.zeros(img.shape)
 
-	for row in range(img_row):
-		for col in range(img_col):
+    for row in range(img_row):
+        for col in range(img_col):
             # Dot product of the filter and the image.
-			filtered = padded_img[
-				row: row + kernel_row,
-				col: col + kernel_col
-				]
+            filtered = padded_img[
+                row: row + kernel_row,
+                col: col + kernel_col
+            ]
             # Sum of the values of the dot product for each position.
-			output[row, col] = np.sum(kernel * filtered)
+            output[row, col] = np.sum(kernel * filtered)
 
-    # Saves the result of the filter.
-	cv2.imwrite("output_image.jpg", output)
+    # Saves the result of the filter on specified filename.
+    cv2.imwrite(filename, output)
 
     # If display is defined, it will show the result.
-	if verbose:
-		plt.imshow(img, cmap='gray')
-		plt.title("Image")
-		plt.show()
+    if verbose:
+        plt.imshow(img, cmap='gray')
+        plt.title("Image")
+        plt.show()
 
-		plt.imshow(padded_img, cmap='gray')
-		plt.title("Padded Image")
-		plt.show()
+        plt.imshow(padded_img, cmap='gray')
+        plt.title("Padded Image")
+        plt.show()
 
-		plt.imshow(output, cmap='gray')
-		plt.title("Output Image using {}X{} Kernel".format(
-			kernel_row, kernel_col))
-		plt.show()
+        plt.imshow(output, cmap='gray')
+        plt.title("Output Image using {}X{} Kernel".format(
+            kernel_row, kernel_col))
+        plt.show()
 
     # Returns the output matrix.
-	return output
-
+    return output, img
 
 if __name__ == '__main__':
-	# Takes the arguments for usage.
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--image", required=True, help="Path to the image")
-	args = vars(ap.parse_args())
-	# use open cv 2 to change the image into an array of numbers
-	image = cv2.imread(args["image"])
+    # Takes the arguments for usage.
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="Path to the image")
+    ap.add_argument("-v", "--verbose", required=False, help="Visualization")
+    args = vars(ap.parse_args())
+    # Use open cv 2 to change the image into an array of numbers
+    image = cv2.imread(args["image"])
 
-	# Filters / Kernel
-	#Edge Detection Kernel
-	edge_detection = np.array([
-		[-1, -1, -1],
-		[-1,  8, -1],
-		[-1, -1, -1]
-		])
-    #Gaussian Blur Kernel
-	gaussian_blur = np.array([
-		[0,  0,   0,   5,   0,  0, 0],
-		[0,  5,  18,  32,  18,  5, 0],
-		[0, 18,  64, 100,  64, 18, 0],
-		[5, 32, 100, 100, 100, 32, 5],
-		[0, 18,  64, 100,  64, 18, 0],
-		[0,  5,  18,  32,  18,  5, 0],
-		[0,  0,   0,   5,   0,  0, 0]
-		])
-	image_gb = convolution(image, gaussian_blur, True)
-	image_ed = convolution(image, edge_detection, True)
+    # Defines if the convolution will display
+    if args["verbose"]=="False": 
+        verbose = False
+    else: 
+        verbose = True
+
+    # Filters / Kernel
+    # Edge Detection Kernel
+    edge_detection = np.array([
+        [-1, -1, -1],
+        [-1,  8, -1],
+        [-1, -1, -1]
+    ])
+
+    # Gaussian Blur Kernel
+    gaussian_blur = np.array([
+        [0,  0,   0,   5,   0,  0, 0],
+        [0,  5,  18,  32,  18,  5, 0],
+        [0, 18,  64, 100,  64, 18, 0],
+        [5, 32, 100, 100, 100, 32, 5],
+        [0, 18,  64, 100,  64, 18, 0],
+        [0,  5,  18,  32,  18,  5, 0],
+        [0,  0,   0,   5,   0,  0, 0]
+    ])
+    
+    # Simple Blur Kernel
+    simple_blur = np.array([
+        [1/9, 1/9, 1/9],
+        [1/9, 1/9, 1/9],
+        [1/9, 1/9, 1/9]
+    ])
+
+    # Normalize the gaussian_blur kernel
+    gaussian_blur = gaussian_blur / np.sum(gaussian_blur)
+
+    # Convolutes with the 3 kernels we provided and saves the output
+    image_gb, grayscale = convolution(image, gaussian_blur, verbose, "Output_gaussianBlur.jpg")
+    image_ed, _ = convolution(grayscale, edge_detection, verbose, "Output_edgeDetection.jpg")
+    image_sb, _ = convolution(grayscale, simple_blur, verbose, "Output_simpleBlur.jpg")
